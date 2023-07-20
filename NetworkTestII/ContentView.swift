f1d59dacf1b66c99f151b08e9f045de4d8262ee1
Kal <kal.aalrajhi@gmail.com>
2023-07-20 16:26:57 -0400

    Initial Commit

---
+++ NetworkTestII/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  NetworkTestII
+//  Using the Combine framework for network calls
+//  Created by Dr Cpt Blackbeard on 7/20/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ NetworkTestII/NetworkTestIIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkTestIIApp.swift
+//  NetworkTestII
+//
+//  Created by Dr Cpt Blackbeard on 7/20/23.
+//
+
+import SwiftUI
+
+@main
+struct NetworkTestIIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
